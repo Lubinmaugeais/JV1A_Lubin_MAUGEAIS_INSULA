84d9fcf8b5ee90d792731e578168012a40132bab
Lubinmaugeais <118370288+Lubinmaugeais@users.noreply.github.com>
2023-04-12 00:10:34 +0200

    Add files via upload

--- zelda like/Sprite-0002.tsx
+++ zelda like/Sprite-0002.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="Sprite-0002" tilewidth="32" tileheight="32" tilecount="10000" columns="100">
- <image source="Documents/Sprite-0002.png" width="3200" height="3200"/>
+ <image source="Sprite-0002.png" width="3200" height="3200"/>
 </tileset>
--- zelda like/zelda like tiled.tsx
+++ zelda like/zelda like tiled.tsx
@@ -1,4 +1,157 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tileset zelda like" tilewidth="32" tileheight="32" tilecount="3025" columns="46">
- <image source="Downloads/tileset zelda like.png" width="1500" height="1500"/>
+<tileset version="1.9" tiledversion="1.9.2" name="tileset zelda like" tilewidth="32" tileheight="32" tilecount="10000" columns="100">
+ <editorsettings>
+  <export target="zelda like tiled..tsj" format="json"/>
+ </editorsettings>
+ <image source="Sprite-0002.png" width="3200" height="3200"/>
+ <tile id="0">
+  <properties>
+   <property name="estsolide" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="4638">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4836">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4838">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4840">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4843">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4848">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4850">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4852">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4855">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5035">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5037">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5039">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5041">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5043">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5045">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5047">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5049">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5051">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5053">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5055">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5057">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5236">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5247">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5251">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5257">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5259">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5544">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5549">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="5649">
+  <properties>
+   <property name="estsolide" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
